8402204e261b1f67a61279b8ef9bb942c1d02f3c
Raghav <142162663+Raghav-56@users.noreply.github.com>
2025-05-11 13:19:04 +0530

    feat: update branding in sidebar and clean up nav-main component
    - Changed company name in AppSidebar from "Acme Inc." to "Hirelytics".
    - Removed unused icons and "Quick Create" button from NavMain for cleaner navigation.

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import {
-  IconCirclePlusFilled,
-  IconDashboard,
-  IconMail,
-} from "@tabler/icons-react";
+import { IconDashboard } from "@tabler/icons-react";
 
-import { Button } from "@/components/ui/button";
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -74,25 +69,6 @@ export function NavMain() {
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
-        <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
-            <Button
-              size="icon"
-              className="size-8 group-data-[collapsible=icon]:opacity-0"
-              variant="outline"
-            >
-              <IconMail />
-              <span className="sr-only">Inbox</span>
-            </Button>
-          </SidebarMenuItem>
-        </SidebarMenu>
         <SidebarMenu>
           {menu.map((item) => (
             <SidebarMenuItem
